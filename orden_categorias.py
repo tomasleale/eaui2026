b8c2b0bcc1f1fa1c73994228ed5dfa71844ad4e6
Tomás Leal Elgueda <tomasleal@gmail.com>
2026-05-28 17:54:44 -0400

    cambios

---
+++ orden_categorias.py
@@ -0,0 +1,23 @@
+# Diccionario con órdenes para variables usadas en el notebook
+ORDEN_CATEGORIAS = {
+    "nivel_habilidades": ["Sin habilidades", "Básico", "Intermedio", "Avanzado"],
+    "sexo": ["Hombre", "Mujer"],
+    "tramo_edad": ["Menor de 18", "18-29", "30-44", "45-59", "60 y más"],
+    "gse": ["AB", "C1", "C2", "C3", "D", "E"],
+    "zona": ["Urbana", "Rural"],
+    "educ_jh": [
+        "Básica incompleta", "Básica completa", "Media CH incompleta", "Media CH completa",
+        "Superior universitaria incompleta", "Superior universitaria completa"
+    ],
+    "ocupacion_jh": [
+        "Trabajos ocasionales e informales", "Oficio menor - obrero no calificado",
+        "Obrero calificado - microempresario", "Empleado medio - técnico - prof. independiente",
+        "Ejecutivo medio - prof. universitario", "Alto ejecutivo - empresario - directivo"
+    ],
+    "ingreso_tramo": ["0-199", "200-499", "500-799", "800+"] ,
+    "ingreso_grupo": ["Bajo","Medio","Alto"],
+    "acceso_internet_hogar": ["No","Sí"],
+    "tipo_acceso_fijo": ["ADSL","Fibra","Cable","Otro"],
+    "tipo_plan": ["Prepago","Pospago"],
+    "tipo_acceso_mas_usado": ["Móvil","Fijo"]
+}
